8f1ab46008ee25503aadc0365d740978cd8adce2
shahriar_Nafiz <msnafiz.bd@gmail.com>
2023-08-25 17:52:47 +0600

    customer controller error update

--- BackEnd/app/Http/Controllers/CustomersController.php
+++ BackEnd/app/Http/Controllers/CustomersController.php
@@ -59,7 +59,7 @@ class CustomersController extends Controller
             ]);
 
             return response()->json(['data' => $new_customer, 'message' => "Customer Created Successfully"], 200);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 500);
         }
     }
@@ -104,7 +104,7 @@ class CustomersController extends Controller
             ]);
             $new_customer->refresh();
             return response()->json(['data' => $new_customer, 'message' => "Customer Created Successfully"], 200);
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 500);
         }
     }
