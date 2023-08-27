695d98a3d68233bcd5b2d6f9827219c271ca52f3
devbipu <devbipu@gmail.com>
2023-08-28 00:56:34 +0600

    Billing & Customer Address Resource added

---
+++ BackEnd/app/Http/Resources/BillingResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BillingResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id"               => $this->id,
+            "bill_amount"      => $this->bill_amount,
+            "addtional_charge" => $this->addtional_charge,
+            "discount"         => $this->discount,
+            "vat"              => $this->vat,
+            "advance"          => $this->advance,
+            "billing_month"    => $this->billing_month,
+            "dues"             => $this->dues,
+            "customer_id"      => $this->customer_id,
+        ];
+    }
+}
